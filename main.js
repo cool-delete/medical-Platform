23bddd92a3f161a81e2345109e89e00ec584f44b
cool-delete <kk520149492@gmail.com>
2019-03-29 22:38:13 +0800

    已安装数据库 尚未安装node跟mongod的通讯方法

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+//模块是用来控制应用的生命周期还有创建本地原生窗口
+const { app, BrowserWindow } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
+//保持一个windows对象的全局引用 如果不这样做 当js对象被垃圾回收时候窗口也会被销毁
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
+  //创建一个浏览窗口
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
+      //为窗口注入node相关api,默认值为true 
       nodeIntegration: true
     }
   })
 
   // and load the index.html of the app.
+
+  //为该应用加载index.html
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
+  //当关闭窗口时清空对象
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
@@ -33,13 +40,16 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
+//createWindow方法会当ele初始化完成还有准备创建浏览器窗口的时候被调用
+//部分api会在ready事件发生后才能被使用
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
+//当所有窗口被关闭的时候退出
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (process.platform !== 'darwin') {//不是mac情况下可以直接退出
     app.quit()
   }
 })
